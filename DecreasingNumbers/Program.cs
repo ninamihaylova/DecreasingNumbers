4b726020828cd86ca546ccd6372cd93a4d587c58
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-16 23:31:28 +0300

    Initial commit

---
+++ DecreasingNumbers/Program.cs
@@ -0,0 +1,13 @@
+﻿class DecreasingNumbers
+{
+    public static void Main(string[] args)
+    {
+        int number = int.Parse(Console.ReadLine());
+        while (number>=1)
+        {
+            Console.WriteLine(number);
+            number--;
+
+        }
+    }
+}
